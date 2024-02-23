e2cc23ee89d2830ae44062c67bd44f450d6059e9
K BHARATH KUMAR <112929839+kalikiri-bharath06@users.noreply.github.com>
2024-02-23 08:54:01 +0530

    Create class with member function.cpp

---
+++ class with member function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+};
+
+int main() {
+    Calculator calc;
+    std::cout << "Addition: " << calc.add(10, 5) << std::endl;
+    std::cout << "Subtraction: " << calc.subtract(10, 5) << std::endl;
+    return 0;
+}
